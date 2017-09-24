53ca0212af2a3d8036af73966184b83088b0533c
Mike Waddell <waddellmusic@gmail.com>
2017-09-24 19:10:06 +0000

    added control for paddles, errors present

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,37 @@
 var canvas = document.getElementById('pong');
 var context = canvas.getContext('2d');
 
-context.rect(0, 0, canvas.width, canvas.height);
+var computer = new Rectangle(20, 20);
+var player = new Rectangle(860, 470);
+var ball = new Rectangle(440, 290);
 
-var gradient = context.createLinearGradient(0, 300, canvas.width, 300);
-gradient.addColorStop(0, 'lightgrey');
-gradient.addColorStop(0.5, 'white');
-gradient.addColorStop(1, 'lightgrey');
-context.fillStyle = gradient;
-context.fill();
-
-function Computer(x, y, width, height) {
+function Rectangle(x, y) {
   this.x = x;
   this.y = y;
-  this.width = width;
-  this.height = height;
+  this.speed = 20;
   this.render = function() {
-    var canvas = document.getElementById('pong');
-    var context = canvas.getContext('2d');
-    context.fillStyle = 'green';
-    context.fillRect(x, y, width, height);
-  }
+    context.fillStyle = 'darkred';
+    context.fillRect(x, y, 20, 110);
+  };
+  this.moveUp = function() {
+    if (y >= 10) {
+      y -= this.speed;
+      step();
+      context.fillRect(x, y, 20, 110);
+    };
+  };
+  this.moveDown = function() {
+    if (y <= 470) {
+      y += this.speed;
+      step();
+      context.fillRect(x, y, 20, 110);
+    };
+  };
 };
 
-function Player(x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.render = function() {
-    var canvas = document.getElementById('pong');
-    var context = canvas.getContext('2d');
-    context.fillStyle = 'purple';
-    context.fillRect(x, y, width, height);
-  }
+ball.render = function() {
+  context.fillRect(this.x, this.y, 20, 20);
 };
-
-function Ball(x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.render = function() {
-    var canvas = document.getElementById('pong');
-    var context = canvas.getContext('2d');
-    context.fillStyle = 'black';
-    context.fillRect(x, y, width, height);
-  }
-};
-
-var computer = new Computer(0, 0, 10, 100);
-var player = new Player(890, 500, 10, 100);
-var ball = new Ball(440, 290, 20, 20);
 
 var render = function() {
   computer.render();
@@ -59,6 +39,38 @@ var render = function() {
   ball.render();
 };
 
-window.onload = function() {
+var step = function() {
+  clear();
   render();
+  animate;
+};
+
+var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+  function(step) {
+    window.setTimeout(step, 1000/60);
+  };
+
+var clear = function() {
+  context.rect(0, 0, canvas.width, canvas.height);
+  var gradient = context.createLinearGradient(0, 300, canvas.width, 300);
+  gradient.addColorStop(0, 'lightgrey');
+  gradient.addColorStop(0.5, 'white');
+  gradient.addColorStop(1, 'lightgrey');
+  context.fillStyle = gradient;
+  context.fill();
+};
+
+var move = function(e) {
+  if (e.keyCode == 104) {
+    player.moveUp();
+  }
+  else if (e.keyCode == 98) {
+    player.moveDown();
+  }
+};
+
+window.addEventListener('keydown', move, true);
+
+window.onload = function() {
+  step();
 };
